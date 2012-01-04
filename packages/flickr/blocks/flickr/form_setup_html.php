909ebea033c57beb303349dbf8cb8794e318e4d4
Matt Waters <matt@concretecms.com>
2012-01-03 18:28:19 -0800

    added spacing on forms

--- packages/flickr/blocks/flickr/form_setup_html.php
+++ packages/flickr/blocks/flickr/form_setup_html.php
@@ -15,21 +15,21 @@ table#FlickrSetup td{ font-size:12px }
 	</tr>
 	<tr>
 		<th><?php echo t('Title')?>: (<?php echo t('Optional')?>)</th>
-		<td><input id="ccm_flickr_title" name="title" value="<?php echo $controllerObj->title?>" maxlength="255" type="text" style="width:100%"></td>
+		<td style="padding-top: 5px"><input id="ccm_flickr_title" name="title" value="<?php echo $controllerObj->title?>" maxlength="255" type="text" style="width:100%"></td>
 	</tr>	
 	<tr>
 		<th><?php echo t('Number of images to display')?>:</th>
-		<td><input id="ccm_flickr_itemsToDisplay"  name="itemsToDisplay" value="<?php echo intval($controllerObj->itemsToDisplay)?>" type="text" size="2" maxlength="3"></td>
+		<td style="padding-top: 5px"><input id="ccm_flickr_itemsToDisplay"  name="itemsToDisplay" value="<?php echo intval($controllerObj->itemsToDisplay)?>" type="text" size="2" maxlength="3"></td>
 	</tr>
 	<tr>
 		<th><?php echo t('Max Width')?>:</th>
-		<td>
+		<td style="padding-top: 5px">
 			<input id="ccm_flickr_max_width" name="maxWidth" value="<?php echo $controllerObj->maxWidth?>" maxlength="8" type="text" >
 		</td>
 	</tr>
 	<tr>
 		<th><?php echo t('Max Height')?>:</th>
-		<td>
+		<td style="padding-top: 5px">
 			<input id="ccm_flickr_max_height" name="maxHeight" value="<?php echo $controllerObj->maxHeight?>" maxlength="8" type="text" >
 		</td>
 	</tr> 		
